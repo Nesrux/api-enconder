d91ef69b4b9f237188667aea33f2c13209af5e1c
Nesrux <joaomarcosdevs@gmail.com>
2024-05-15 13:35:51 -0300

    feat: adiciona validação e métodos ao job

--- domain/job.go
+++ domain/job.go
@@ -1,13 +1,53 @@
 package domain
 
-import "time"
+import (
+	"time"
+
+	"github.com/asaskevich/govalidator"
+	"github.com/google/uuid"
+)
+
+func init() {
+	govalidator.SetFieldsRequiredByDefault(true)
+}
 
 type Job struct {
-	ID               string
-	OutputBucketPath string
-	Status           string
-	Video            *Video
-	Error            string
-	CreatedAt        time.Time
-	UpdatedAt        time.Time
+	ID               string    `valid:"uuid"`
+	OutputBucketPath string    `valid:"notnull"`
+	Status           string    `valid:"notnull"`
+	Video            *Video    `valid:"-"`
+	VideoId          string    `valid:"-"`
+	Error            string    `valid:"-"`
+	CreatedAt        time.Time `valid:"notnull"`
+	UpdatedAt        time.Time `valid:"notnull"`
+}
+
+func NewJob(output, status string, video *Video) (*Job, error) {
+	job := Job{
+		OutputBucketPath: output,
+		Status:           status,
+		Video:            video,
+	}
+	job.prepare()
+
+	err := job.Validate()
+
+	if err != nil {
+		return nil, err
+	}
+	return &job, nil
+}
+
+func (j *Job) prepare() {
+	j.ID = uuid.NewString()
+	j.CreatedAt = time.Now()
+	j.UpdatedAt = time.Now()
+}
+
+func (j *Job) Validate() error {
+	_, err := govalidator.ValidateStruct(j)
+	if err != nil {
+		return err
+	}
+	return nil
 }
