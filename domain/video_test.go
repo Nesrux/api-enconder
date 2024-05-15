8ade60a15858b6b95ea522b7ba29ddf5f1230a4e
Nesrux <joaomarcosdevs@gmail.com>
2024-05-15 13:17:23 -0300

    test: adiciona mais testes a entidade de video

--- domain/video_test.go
+++ domain/video_test.go
@@ -1,8 +1,10 @@
 package domain_test
 
 import (
-	"github.com/Nesrux/api-enconder/domain"
 	"testing"
+	"time"
+
+	"github.com/Nesrux/api-enconder/domain"
 
 	"github.com/stretchr/testify/require"
 )
@@ -12,5 +14,18 @@ func Test_Validated_ifVideoIsEmpty(t *testing.T) {
 	err := video.Validate()
 
 	require.Error(t, err)
+}
 
+func Test_VideoId_IsNotUUID(t *testing.T) {
+	video := domain.NewVideo()
+	//when
+	video.ID = "abc"
+	video.ResourceID = "123"
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+	
+	//then 
+	err := video.Validate()
+
+	require.Error(t, err)
 }
