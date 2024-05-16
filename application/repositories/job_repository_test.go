4b9e5b90ba952f219ef09b2bce9993eef642a54f
Nesrux <joaomarcosdevs@gmail.com>
2024-05-16 12:48:47 -0300

    test: adiciona teste de update de  job

--- application/repositories/job_repository_test.go
+++ application/repositories/job_repository_test.go
@@ -32,3 +32,30 @@ func Test_JobRepository_DbInsert(t *testing.T) {
 	require.Equal(t, j.VideoID, video.ID)
 
 }
+
+func Test_JobRepository_DbUpdate(t *testing.T) {
+	db := database.NewDbTest()
+	defer db.Close()
+
+	video := domain.NewVideo()
+	video.FilePath = "path"
+
+	repo := repositories.VideoRepositoryDb{Db: db}
+	repo.Insert(video)
+
+	job, err := domain.NewJob("output_path", "Pending", video)
+	require.Nil(t, err)
+
+	repoJob := repositories.JobRepositoryDb{Db: db}
+	repoJob.Insert(job)
+
+	job.Status = "Complete"
+	repoJob.Update(job)
+
+	j, err := repoJob.Find(job.ID)
+	require.NotEmpty(t, j.ID)
+	require.Nil(t, err)
+	require.Equal(t, j.ID, job.ID)
+	require.Equal(t, j.Status, job.Status)
+
+}
