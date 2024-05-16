306dfb8c0a6a37a75ce5f17f832ecc1c3db7f766
Nesrux <joaomarcosdevs@gmail.com>
2024-05-16 11:16:10 -0300

    feat: configura o banco de dados

--- framework/database/db.go
+++ framework/database/db.go
@@ -53,6 +53,8 @@ func (d *Database) Connect() (*gorm.DB, error) {
 	}
 	if d.AutoMigrateDb {
 		d.Db.AutoMigrate(&domain.Video{}, &domain.Job{})
+		//cria chave estrangeira
+		d.Db.Model(domain.Job{}).AddForeignKey("video_id", "videos (id)", "CASCADE", "CASCADE")
 	}
 	return d.Db, nil
 }
