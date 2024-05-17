810ba69411d3dde956c4f6686de14261ec4755ba
Nesrux <joaomarcosdevs@gmail.com>
2024-05-17 14:27:22 -0300

    test: adiciona teste para download de video

---
+++ application/services/video_service_test.go
@@ -0,0 +1,45 @@
+package services_test
+
+import (
+	"log"
+	"testing"
+
+	"github.com/Nesrux/api-enconder/application/repositories"
+	"github.com/Nesrux/api-enconder/application/services"
+	"github.com/Nesrux/api-enconder/domain"
+	"github.com/Nesrux/api-enconder/framework/database"
+	"github.com/joho/godotenv"
+	"github.com/stretchr/testify/require"
+)
+
+func init() {
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+}
+
+func Test_VideoService_download(t *testing.T) {
+	video, repo := prepare()
+
+	videoService := services.NewVideoService()
+	videoService.Video = video
+	videoService.VideoRepository = repo
+
+	err := videoService.Downlaod("bucket-encoder")
+	require.Nil(t, err)
+
+}
+
+func prepare() (*domain.Video, repositories.VideoRepositoryDb) {
+	db := database.NewDbTest()
+	defer db.Close()
+
+	video := domain.NewVideo()
+	video.FilePath = "videotest.mp4"
+
+	repo := repositories.VideoRepositoryDb{Db: db}
+	repo.Insert(video)
+
+	return video, repo
+}
