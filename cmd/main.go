a7d636291a2ce62318ffc0140a8bd469d74ec2ea
Nesrux <joaomarcosdevs@gmail.com>
2024-05-18 20:38:51 -0300

    feat: add func main

---
+++ cmd/main.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	/*TODOS :
+	1) atualizar o projeto para versão mais recente da Golang
+	2) criar uma pepilane de CI/CD para buildar o conteiner
+	3) adicionar métodos para não usar o google Cloud sotrage e deixar tudo localmente
+	4) remover dependencias obsoletas
+	5) adicionar mais testes e modificar os já existentes*/
+}
