10662b310cd151cc52027c469e51e321b54a9d1e
Nesrux <joaomarcosdevs@gmail.com>
2024-05-21 20:53:10 -0300

    chore: adiciona nova propertie

--- application/services/video_service.go
+++ application/services/video_service.go
@@ -13,7 +13,8 @@ import (
 )
 
 const (
-	LOCAL_STORAGE_PATH = "localStoragePath"
+	LOCAL_STORAGE_PATH      = "localStoragePathTest"
+	LOCAL_STORAGE_PATH_TEST = "lovalStoragePathTest"
 )
 
 type VideoService struct {
