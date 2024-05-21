b3ee1b84191a788c18f53bbeb2d5ae09269887d3
Nesrux <joaomarcosdevs@gmail.com>
2024-05-21 20:38:19 -0300

    test: adiciona teste de upload manager

---
+++ application/services/upload_manager_test.go
@@ -0,0 +1,45 @@
+package services_test
+
+import (
+	"log"
+	"os"
+	"testing"
+
+	"github.com/Nesrux/api-enconder/application/services"
+	"github.com/joho/godotenv"
+	"github.com/stretchr/testify/require"
+)
+
+func init() {
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+}
+
+func Test_videoservice_upload(t *testing.T) {
+	video, repo := prepare()
+
+	videoService := services.NewVideoService()
+	videoService.Video = video
+	videoService.VideoRepository = repo
+
+	err := videoService.Downlaod("bucket-encoder")
+	require.Nil(t, err)
+
+	err = videoService.Fragment()
+	require.Nil(t, err)
+
+	err = videoService.Enconde()
+	require.Nil(t, err)
+
+	videoUpload := services.NewVideoUpload()
+	videoUpload.OutputBucket = "bucket-encoder"
+	videoUpload.VideoPath = os.Getenv(services.LOCAL_STORAGE_PATH) + "/" + video.ID
+
+	doneUpload := make(chan string)
+	go videoUpload.ProcessUpload(30, doneUpload)
+
+	result := <-doneUpload
+	require.Equal(t, result, "upload completed")
+}
