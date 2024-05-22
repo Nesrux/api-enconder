bdc7942fbca7d739db404122d8d5d56071ac0758
Nesrux <joaomarcosdevs@gmail.com>
2024-05-22 18:35:09 -0300

    feat: adiciona implementação de jobservice

--- application/services/job_service.go
+++ application/services/job_service.go
@@ -1,8 +1,18 @@
 package services
 
 import (
+	"errors"
+	"os"
+	"strconv"
+
 	"github.com/Nesrux/api-enconder/application/repositories"
 	"github.com/Nesrux/api-enconder/domain"
+)
+
+const (
+	localStoragePath  = "localStoragePath"
+	inputBucketName   = "inputBucketName"
+	concurrency_upload= "concurrency_upload"
 )
 
 type JobService struct {
@@ -11,8 +21,82 @@ type JobService struct {
 	VideoService  VideoService
 }
 
-func (j *JobService) start() error {
+func (j *JobService) Start() error {
+	err := j.changeJobStatus("DOWNLOADING")
+
+	if err != nil {
+		return j.failJob(err)
+	}
+	err = j.VideoService.Downlaod(os.Getenv(inputBucketName))
+
+	if err != nil {
+		return j.failJob(err)
+	}
+
+	err = j.changeJobStatus("FRAGMENTING")
+
+	if err != nil {
+		return j.failJob(err)
+	}
+	err = j.VideoService.Fragment()
+
+	if err != nil {
+		return j.failJob(err)
+	}
+
+	err = j.changeJobStatus("ENCONDING")
+	if err != nil {
+		return j.failJob(err)
+	}
+	err = j.VideoService.Enconde()
+
+	if err != nil {
+		return j.failJob(err)
+	}
+	err = j.performUpload()
+	if err != nil {
+		return j.failJob(err)
+	}
+	err = j.changeJobStatus("FiNISHING")
+	if err != nil {
+		return j.failJob(err)
+	}
+	err = j.VideoService.Finish()
+	if err != nil {
+		return j.failJob(err)
+	}
+
+	err = j.changeJobStatus("COMPLETED")
+	if err != nil {
+		return j.failJob(err)
+	}
+
 	return nil
+}
+
+func (j *JobService) performUpload() error {
+	err := j.changeJobStatus("UPLOADING")
+
+	if err != nil {
+		return j.failJob(err)
+	}
+
+	videoUpload := NewVideoUpload()
+	videoUpload.OutputBucket = os.Getenv(inputBucketName)
+	videoUpload.VideoPath = os.Getenv(localStoragePath) + "/" + j.VideoService.Video.ID
+	concurrency, _ := strconv.Atoi(os.Getenv(concurrency_upload))
+	doneUpload := make(chan string)
+
+	go videoUpload.ProcessUpload(concurrency, doneUpload)
+
+	var uploadResult string
+	uploadResult = <-doneUpload
+
+	if uploadResult != "upload completed" {
+		return j.failJob(errors.New(uploadResult))
+	}
+
+	return err
 }
 
 func (j *JobService) changeJobStatus(status string) error {
